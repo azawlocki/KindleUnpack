76c0b318bdce47ccadaed2e21b87e0f2203d0377
Artur Zawłocki <artur.zawlocki@imapp.pl>
2016-04-13 23:26:03 +0200

    basic setup script
    `setup.py install` installs modules from the `lib/` dir
    into the package `kindleunpack`. Other modules are ignored.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kindleunpack',
+      version='0.80',
+      packages=['kindleunpack'],
+      package_dir={'kindleunpack': 'lib'},
+  )
+      
+      
